dc0d3d28702f2ffda8a689807ce0f0be7dde8a8b
insertSoul <j.clarke213@gmail.com>
2022-11-06 17:40:47 +0000

    Add multiple turns to the game

--- battleshipsDebug.py
+++ battleshipsDebug.py
@@ -9,8 +9,6 @@ def print_board(board):
   for row in board:
     print (" ".join(row))
 
-print_board(board)
-
 def random_row(board):
   return randint(0, len(board) - 1)
 
@@ -19,20 +17,27 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
+
+# for testing purposes, print actual location
 print (ship_row)
 print (ship_col)
 
-guess_row = int(input("Guess Row: "))
-guess_col = int(input("Guess Col: "))
+print ("\nYou have Six Guess to find the ship \n")
+print_board(board)
+
 
 # Write your code below!
-if guess_row == ship_row and guess_col == ship_col:
-  print ("Congratulations! You sank my battleship!")
-elif guess_row not in range(5) and guess_col not in range(5):
-  print ("Oops, that's not even in the ocean.")
-elif board[guess_row][guess_col] == "X":
-  print ("You guessed that one already.")
-else:
-    print ("You missed my battleship!")
-    board[guess_row][guess_col] = "X"
-    print_board(board)
+for i in range (6):
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
+  if guess_row == ship_row and guess_col == ship_col:
+    print ("Congratulations! You sank my battleship!")
+  elif guess_row not in range(5) and guess_col not in range(5):
+    print ("Oops, that's not even in the ocean.")
+  elif board[guess_row][guess_col] == "X":
+    print ("You guessed that one already.\n you'v lost a turn")
+  else:
+      print ("You missed my battleship!")
+      board[guess_row][guess_col] = "X"
+      print_board(board)
+print("\nYou didn't hit my battleship and are out of guesses\n")
