84a4044a4fc7cb6a6773ff67611716c7a498f6f3
insertSoul <j.clarke213@gmail.com>
2022-11-08 18:09:52 +0000

    Create a func to add items to inventory

--- fantasyGameInventory.py
+++ fantasyGameInventory.py
@@ -1,15 +1,34 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+from collections import Counter
 
 def displayInventory(inventory):
     print("Inventory:")
-    item_total = 0 # item countter
+    item_total = 0 # item counter
     for k, v in inventory.items():   # loop trough inventory
         print(str(v) + ' ' + k )     # print the value a space and the item
         item_total += 1 * v          # add the items to the total
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+def addToInventory(inventory, addItems):
+    #itemsDict = Counter(addItems) # turn items into dictionary (not needed)
+    for i in addItems: # run through addItems (the loot obtained)
+        inventory.setdefault(i, 0) #if an unkown item add to the inventory
+        inventory[i] += 1 # add the item to the inventory
+    return(inventory)
+
+inv = {'gold coin': 42, 'rope': 1}
+print("Starting inventory is :\n")
+displayInventory(inv)
 
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-#Loot comes in a list, needs turning into dict. to add it in
+inv = addToInventory(inv, dragonLoot)
+print("\nFinal inventory is :\n")
+displayInventory(inv)
 
+#### MORE TESTING ##########
+
+#inv2 = {'gold coin' : 12 , 'Axe' : 1 , 'helmet' : 2  , 'sword': 3 , 'magic frog' : 69}
+#treasureChest = {'gold coin' , 'gold coin' ,'gold coin' ,'gold coin' ,'gold coin' ,'cape of destruction'}
+#treasureChest2 = {  (('gold coin') )*10 ,'cape of destruction'}
+
+#inv2 = addToInventory(inv2, treasureChest2)
+#displayInventory(inv2)
